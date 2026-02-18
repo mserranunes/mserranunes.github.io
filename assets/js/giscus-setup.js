039fdcd5b56f68f45cf94104f7d524ced4a5e584
mserranunes <manuelserranunes@gmail.com>
2026-02-18 15:43:08 +0000

    Deploying to gh-pages from @ mserranunes/mserranunes.github.io@8aa91d395ac7834a618a621356d26a244345ac76 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "m-serra/m-serra.github.io",
+    "data-repo": "mserranunes/mserranunes.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
